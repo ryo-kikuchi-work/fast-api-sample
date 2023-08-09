c55750f33ce8e071f8b33d0dabf74709f62ee496
kr <mf23041@shibaura-it.ac.jp>
2023-08-09 17:05:24 +0900

    ログイントークン処理の追加

--- script/routers/auth.py
+++ script/routers/auth.py
@@ -1,12 +1,18 @@
 import hashlib
 from datetime import datetime, timedelta
-from jose import JWTError, jwt
+from jose import jwt
 from fastapi import APIRouter, Depends, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from pydantic import BaseModel
-from fastapi.responses import JSONResponse, HTMLResponse, Response
+from fastapi.responses import JSONResponse, Response
 
-from script import json_db
+from script.DB import json_db
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
+
+# jose jwt
+ALGORITHM = "HS256"
+KEY = "KEY"
 
 
 class Message(BaseModel):
@@ -14,26 +20,46 @@ class Message(BaseModel):
 
 
 class auth_token(BaseModel):
-    exp: int
-    token: str
-    sub_token: str
+    token_type: str
+    access_token: str
+
+
+class auth_info(BaseModel):
+    user_id: str = ""
+    auth: bool = False
+    permission: int = "-1"
+
+    def __init__(self, user_id, auth, permission):
+        super().__init__()
+
+        self.user_id = user_id
+        self.auth = auth
+        self.permission = permission
 
 
 router = APIRouter()
 
-user_db = json_db.DB("users.json")
+user_db = json_db.DB("DB/users.json")
 
 
-def create_access_token(data: dict, expires_delta: timedelta = None):
+def create_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)  # 15分で有効期限が切れるトークンを生成
+        expire = datetime.utcnow() + timedelta(minutes=15)
 
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, "SECRET_KEY", algorithm="ALGORITHM")
+    encoded_jwt = jwt.encode(to_encode, KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def create_access_token(user_id):
+    user_db.load()
+    token = create_token({"userid": user_db()[user_id]["userid"]}, timedelta(minutes=15))
+    user_db()[user_id]["access_token"] = token
+    user_db.dump()
+    return token
 
 
 @router.post("/users/register/",
@@ -42,6 +68,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
              response_description='None',
              tags=["Auth"],
              responses={200: {"model": Message},
+                        201: {"model": Message},
                         409: {"model": Message}})
 async def create_account(auth_data: OAuth2PasswordRequestForm = Depends()):
     user_db.load()
@@ -51,8 +78,10 @@ async def create_account(auth_data: OAuth2PasswordRequestForm = Depends()):
     else:
         hash_ps = hashlib.sha256(auth_data.password.encode("utf-8")).hexdigest()
         user_db()[user_id] = {
-            "username": auth_data.username,
-            "password": hash_ps
+            "permission": 0,
+            "userid": auth_data.username,
+            "password": hash_ps,
+            "access_token": create_access_token(auth_data.username)
         }
         user_db.dump()
         return Response(status_code=status.HTTP_201_CREATED)
@@ -63,22 +92,50 @@ async def create_account(auth_data: OAuth2PasswordRequestForm = Depends()):
              description='create access token',
              response_description='None',
              tags=["Auth"],
-             responses={201: {"model": Message},
+             responses={201: {"model": auth_token},
                         401: {"model": Message}})
 async def get_access_token(auth_data: OAuth2PasswordRequestForm = Depends()):
-
     user_db.load()
     user_id = hashlib.sha256(auth_data.username.encode("utf-8")).hexdigest()
+    user_pw = hashlib.sha256(auth_data.password.encode("utf-8")).hexdigest()
+
     if user_id in user_db():
-        return Response(status_code=status.HTTP_201_CREATED)
+        if user_db()[user_id]["password"] == user_pw:
+            token = create_access_token(user_id)
+            return JSONResponse(status_code=status.HTTP_201_CREATED,
+                                content={"token_type": "bearer",
+                                         "access_token": token})
 
     return Response(status_code=status.HTTP_401_UNAUTHORIZED)
 
 
-@router.get("/users/login/",
-            summary='login',
-            description='get access token',
-            response_description='None',
-            tags=["Auth"])
-async def get_access_token():
+def token_auth(token: str = Depends(oauth2_scheme)):
+    user_db.load()
+
+    payload = jwt.decode(token, KEY, ALGORITHM)
+
+    user_id = payload["userid"]
+    hash_user_id = hashlib.sha256(user_id.encode("utf-8")).hexdigest()
+
+    user_id = user_id if hash_user_id in user_db() else ""
+    auth = False
+    if datetime.utcnow() .timestamp() < payload["exp"] and user_id != "":
+        if user_db()[hash_user_id]["access_token"] == token:
+            auth = True
+    permission = user_db()[hash_user_id]["permission"] if auth else -1
+
+    if auth:
+        user_db()[hash_user_id]["access_token"] = create_access_token(user_id)
+        user_db.dump()
+    return auth_info(user_id, auth, permission)
+
+
+# @router.get('/user/{user_id}')
+# def get_dict(user_id: str, auth: auth_info = Depends(token_auth)):
+#     user_db.load()
+#     return auth
+
+@router.delete("/user/delete")
+def delete_account():
     pass
+
